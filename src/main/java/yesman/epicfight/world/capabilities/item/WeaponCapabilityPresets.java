677c4f1e32b9c53994ed37e3324113ba34356a39
Zeff <storiomario@gmail.com>
2024-06-05 02:17:02 +0200

    greatsword walk/run animation

--- src/main/java/yesman/epicfight/world/capabilities/item/WeaponCapabilityPresets.java
+++ src/main/java/yesman/epicfight/world/capabilities/item/WeaponCapabilityPresets.java
@@ -140,28 +140,28 @@ public class WeaponCapabilityPresets {
 
 		return builder;
 	};
-	public static final Function<Item, CapabilityItem.Builder> GREATSWORD = (item) -> {
-		WeaponCapability.Builder builder = WeaponCapability.builder()
-				.category(WeaponCategories.GREATSWORD)
-				.styleProvider((playerpatch) -> Styles.TWO_HAND)
-				.collider(ColliderPreset.GREATSWORD)
-				.swingSound(EpicFightSounds.WHOOSH_BIG)
-				.hitSound(EpicFightSounds.BLADE_HIT)
-				.canBePlacedOffhand(false)
-				.newStyleCombo(Styles.TWO_HAND, Animations.GREATSWORD_AUTO1, Animations.GREATSWORD_AUTO2, Animations.GREATSWORD_DASH, Animations.GREATSWORD_AIR_SLASH)
-				.specialAttack(Styles.TWO_HAND, EpicFightSkills.GIANT_WHIRLWIND)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.IDLE, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.WALK, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.CHASE, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.RUN, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.JUMP, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.KNEEL, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.SNEAK, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.SWIM, Animations.BIPED_HOLD_GREATSWORD)
-				.livingMotionModifier(Styles.TWO_HAND, LivingMotions.BLOCK, Animations.GREATSWORD_GUARD);
+	public static final Function<Item, CapabilityItem.Builder> GREATSWORD = (item) -> WeaponCapability.builder()
+            .category(WeaponCategories.GREATSWORD)
+            .styleProvider((playerpatch) -> Styles.TWO_HAND)
+            .collider(ColliderPreset.GREATSWORD)
+            .swingSound(EpicFightSounds.WHOOSH_BIG)
+            .hitSound(EpicFightSounds.BLADE_HIT)
+            .canBePlacedOffhand(false)
+            .newStyleCombo(Styles.TWO_HAND, Animations.GREATSWORD_AUTO1, Animations.GREATSWORD_AUTO2, Animations.GREATSWORD_DASH, Animations.GREATSWORD_AIR_SLASH)
+            .specialAttack(Styles.TWO_HAND, EpicFightSkills.GIANT_WHIRLWIND)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.IDLE, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.WALK, Animations.BIPED_WALK_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.CHASE, Animations.BIPED_WALK_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.RUN, Animations.BIPED_RUN_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.JUMP, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.KNEEL, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.SNEAK, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.SWIM, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.FLY, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.CREATIVE_FLY, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.CREATIVE_IDLE, Animations.BIPED_HOLD_GREATSWORD)
+            .livingMotionModifier(Styles.TWO_HAND, LivingMotions.BLOCK, Animations.GREATSWORD_GUARD);
 
-		return builder;
-	};
 	public static final Function<Item, CapabilityItem.Builder> KATANA = (item) -> {
 		WeaponCapability.Builder builder = WeaponCapability.builder()
 				.category(WeaponCategories.KATANA)
