dfd40a6183562aa2d91173156841d1e67a7861a6
Zeff <storiomario@gmail.com>
2024-06-17 21:14:31 +0200

    new overlay mixin

--- src/main/java/yesman/epicfight/client/gui/screen/overlay/BlendingTextureOverlay.java
+++ src/main/java/yesman/epicfight/client/gui/screen/overlay/BlendingTextureOverlay.java
@@ -1,9 +1,7 @@
+
 package yesman.epicfight.client.gui.screen.overlay;
 
-import org.lwjgl.opengl.GL11;
-
 import com.mojang.blaze3d.platform.GlStateManager;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
@@ -11,32 +9,38 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.lwjgl.opengl.GL11;
 
 @OnlyIn(Dist.CLIENT)
 public class BlendingTextureOverlay extends OverlayManager.Overlay {
 	public ResourceLocation texture;
-	
+	private boolean isAlive = true;
+
 	public BlendingTextureOverlay(ResourceLocation texture) {
 		this.texture = texture;
 	}
-	
+
+	public void remove() {
+		this.isAlive = false;
+	}
+
 	@Override
-	public void render(int xResolution, int yResolution) {
+	public boolean render(int xResolution, int yResolution) {
 		//RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		//RenderSystem.setShaderTexture(0, this.texture);
-		
-		Minecraft.getInstance().getTextureManager().bind(this.texture);
-		
+		Minecraft.getInstance().getTextureManager().bind(this.texture); //TODO remove after
 		GlStateManager._enableBlend();
 		GlStateManager._disableDepthTest();
 		GlStateManager._blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		Tessellator tessellator = Tessellator.getInstance();
-	    BufferBuilder bufferbuilder = tessellator.getBuilder();
-	    bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-	    bufferbuilder.vertex(0, 0, 1).uv(0, 0).endVertex();
-	    bufferbuilder.vertex(0, yResolution, 1).uv(0, 1).endVertex();
-	    bufferbuilder.vertex(xResolution, yResolution, 1).uv(1, 1).endVertex();
-	    bufferbuilder.vertex(xResolution, 0, 1).uv(1, 0).endVertex();
-	    tessellator.end();
+		BufferBuilder bufferbuilder = tessellator.getBuilder();
+		bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+		bufferbuilder.vertex(0, 0, 1).uv(0, 0).endVertex();
+		bufferbuilder.vertex(0, yResolution, 1).uv(0, 1).endVertex();
+		bufferbuilder.vertex(xResolution, yResolution, 1).uv(1, 1).endVertex();
+		bufferbuilder.vertex(xResolution, 0, 1).uv(1, 0).endVertex();
+		tessellator.end();
+
+		return !this.isAlive;
 	}
 }
--- src/main/java/yesman/epicfight/client/gui/screen/overlay/FlickeringOverlay.java
+++ src/main/java/yesman/epicfight/client/gui/screen/overlay/FlickeringOverlay.java
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import yesman.epicfight.client.ClientEngine;
 
 @OnlyIn(Dist.CLIENT)
 public class FlickeringOverlay extends OverlayManager.Overlay {
@@ -11,24 +12,22 @@ public class FlickeringOverlay extends OverlayManager.Overlay {
 	private final float deltaTime;
 	private final float strength;
 	private final double initialGamma;
-	
+
 	public FlickeringOverlay(float deltaTime, float strength) {
 		this.deltaTime = deltaTime;
 		this.strength = strength;
 		Minecraft minecraft = Minecraft.getInstance();
 		this.initialGamma = minecraft.options.gamma;
 	}
-	
+
 	@Override
-	public void render(int xResolution, int yResolution) {
+	public boolean render(int xResolution, int yResolution) {
 		this.time += this.deltaTime;
 		float darkenAmount = MathHelper.clamp((float)Math.sin(this.time), -1.0F, 0.0F);
-		
-		Minecraft minecraft = Minecraft.getInstance();
-		minecraft.options.gamma = this.initialGamma + darkenAmount * strength;
-		
-		if (this.time >= 0) {
-			this.isRemoved = true;
-		}
-	}
+
+		OverlayManager overlayManager = ClientEngine.instance.renderEngine.getOverlayManager();
+		overlayManager.setModifiedGamma(this.initialGamma + darkenAmount * strength);
+
+        return this.time >= 0;
+    }
 }
